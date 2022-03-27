5dcd26cd87f34841970f109ca405dddd09ab51a2
Lucas Neves de Castro Souza <lucas.neves.souza@hotmail.com>
2022-03-27 02:00:46 -0300

    Adding package files

---
+++ Editor/SafeEnumDrawer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SafeEnum.Editor
+{
+	[CustomPropertyDrawer(typeof(SafeEnum<>), true)]
+	public class SafeEnumDrawer : PropertyDrawer
+	{
+		const string ERROR_WARNING_TEXT = "_MISSING_";
+		const int ERROR_INDEX = -1;
+		static readonly Color ERROR_COLOR = Color.red;
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			SerializedProperty enumProperty = property.FindPropertyRelative("_enumValue");
+			bool isErrorDetected = enumProperty.enumValueIndex <= ERROR_INDEX;
+			Color defaultGUIColor = GUI.color;
+
+			label.text = !isErrorDetected ? FirstCharToUpper(label.text) : ERROR_WARNING_TEXT;
+
+			if (isErrorDetected)
+			{
+				GUI.color = ERROR_COLOR;
+			}
+
+			EditorGUI.BeginChangeCheck();
+			EditorGUI.PropertyField(position, enumProperty, label);
+
+			if (EditorGUI.EndChangeCheck())
+			{
+				SerializedProperty stringValueProperty = property.FindPropertyRelative("_stringValue");
+				stringValueProperty.stringValue = enumProperty.enumNames[enumProperty.enumValueIndex];
+			}
+
+			GUI.color = defaultGUIColor;
+		}
+
+		static string FirstCharToUpper(string input)
+		{
+			return string.IsNullOrWhiteSpace(input) ? ERROR_WARNING_TEXT : input[0].ToString().ToUpper() + input.Substring(1);
+		}
+	}
+}
---
+++ Runtime/SafeEnum.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SafeEnum
+{
+	[Serializable]
+	public struct SafeEnum<T> : IEquatable<SafeEnum<T>>, ISerializationCallbackReceiver
+		where T : struct, Enum, IComparable, IConvertible, IFormattable
+	{
+		[SerializeField] string _stringValue;
+		[SerializeField] T _enumValue;
+
+		public T Value { get => _enumValue; set => _enumValue = value; }
+
+		public void OnBeforeSerialize()
+		{
+			_stringValue = _enumValue.ToString();
+		}
+
+		public void OnAfterDeserialize()
+		{
+			if (Enum.TryParse(_stringValue, out T enumValue))
+			{
+				_enumValue = enumValue;
+			}
+			else
+			{
+				int index = (int)(ValueType)_enumValue;
+				if (Enum.IsDefined(typeof(T), index))
+				{
+					_enumValue = (T)(ValueType)index;
+				}
+				else
+				{
+					Debug.LogError($"Deserialization failed: \"{typeof(T).FullName}\" enum has neither \"{_stringValue}\" value, nor \"{index}\" index");
+				}
+			}
+		}
+
+		public override string ToString()
+		{
+			return Enum.GetName(typeof(T), _enumValue);
+		}
+
+		public bool Equals(SafeEnum<T> other)
+		{
+			return EqualityComparer<T>.Default.Equals(_enumValue, other._enumValue);
+		}
+
+		public override int GetHashCode()
+		{
+			return _enumValue.GetHashCode();
+		}
+
+		public static implicit operator T(SafeEnum<T> safeEnum)
+		{
+			return safeEnum._enumValue;
+		}
+
+		public static T Convert(SafeEnum<T> safeEnum)
+		{
+			return safeEnum._enumValue;
+		}
+	}
+}
